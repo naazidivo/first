eb7febbbba478015a714f3aef610aa6527929f62
samir <sawarkar@DESKTOP-RHSUC2J>
2021-12-23 15:32:41 +0530

    new

--- python.py
+++ python.py
@@ -1,1 +1,19 @@
-print('fist githup')
+
+from flask import Flask,request,jsonify
+
+de = {'name': 'samir','age':13}
+app =Flask( "__name__")
+
+
+@app.route('/',methods =['GET'])
+def hello ():
+    
+    name = str(request.args['name'])
+    de.update({f"s{name}": name})
+    return de
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
